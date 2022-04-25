f50457a5cc484951f4b98329e4ed38c37efc7237
Alexander Rusiecki <54362381+alexander-rusiecki@users.noreply.github.com>
2022-04-25 07:20:44 +0200

    split up controllers & install yup & validate username and password

--- server/auth/routes/index.js
+++ server/auth/routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const router = express.Router();
 const useAuth = require('../controllers/authController');
+const useSignup = require('../controllers/signupController');
+
+const router = express.Router();
 
 router.route('/').get(useAuth);
+router.route('/signup').post(useSignup);
 
 module.exports = router;
